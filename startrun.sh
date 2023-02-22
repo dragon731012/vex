b1792fe17295c70822a396561cad6b483b0f8d98
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-02-22 14:58:31 -0600

    Delete startrun.sh

--- startrun.sh
+++
@@ -1,8 +0,0 @@
-# DO NOT RENAME THIS FILE
-NVM_HOME = "$PWD/nvm"
-export NVM_DIR="$NVM_HOME/.nvm" &&
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
-
-nvm install --latest-npm
-bash run.sh
